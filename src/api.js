5b7090fe2e91f0b6795822ccac557906045fdd24
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2019-09-11 21:43:08 -0600

    change something

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const cors =require('cors');
 const app = express();
 const router = express.Router();
 app.use(cors());
+app.use(express.static('public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
